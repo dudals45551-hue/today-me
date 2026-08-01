7c41693e1aa2a6b72d7ecde75b8c5612ac50c194
dudals45551-hue <dudals45551@gmail.com>
2026-08-01 14:45:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const C='today-me-v5-6-1';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
+const CACHE='today-me-v580';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./crown.png','./hat.png','./glasses.png','./dress.png','./cape.png','./wings.png','./heart.png','./star.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
