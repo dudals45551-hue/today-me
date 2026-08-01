6054fef4cdc5f8d454c3598c948701ae28882bea
dudals45551-hue <dudals45551@gmail.com>
2026-08-01 14:15:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='today-me-v5-2-1';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))))
+const C='today-me-v5-6-1';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
