3c20a3b6d0dd0cad8c8ba4468d9822b1e210aac0
dudals45551-hue <dudals45551@gmail.com>
2026-08-01 17:03:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='today-me-v580';
+const CACHE='today-me-v581';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./crown.png','./hat.png','./glasses.png','./dress.png','./cape.png','./wings.png','./heart.png','./star.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
