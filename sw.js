b8a62b2dadf88cc4af4e2dc1a78d23ccfe23a49b
dudals45551-hue <dudals45551@gmail.com>
2026-08-04 01:04:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,27 @@
-const CACHE='today-me-v5-6-2';
-const ASSETS=[
- './',
- './index.html',
- './manifest.json',
- './icons/icon-192.png',
- './icons/icon-512.png',
- './icons/icon-maskable-512.png',
- './icons/icon-180.png'
-];
+const CACHE='harin-seoyul-v621';
+const CORE=['./','./index.html','./style.css','./manifest.json',
+'./icons/app-photo-192-v621.png','./icons/app-photo-512-v621.png',
+'./icons/app-photo-maskable-512-v621.png'];
+
 self.addEventListener('install',event=>{
- event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+ event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
  self.skipWaiting();
 });
+
 self.addEventListener('activate',event=>{
  event.waitUntil(
-   caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
  );
  self.clients.claim();
 });
+
 self.addEventListener('fetch',event=>{
  if(event.request.method!=='GET')return;
  event.respondWith(
-   fetch(event.request).then(response=>{
-     const copy=response.clone();
-     caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-     return response;
-   }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html')))
+  fetch(event.request).then(response=>{
+   const copy=response.clone();
+   caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+   return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
  );
 });
